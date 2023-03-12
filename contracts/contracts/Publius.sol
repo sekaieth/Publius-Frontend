099218995b492a98a1be905dd32ca92f97fd421f
sekaieth <sekaieth@proton.me>
2023-03-12 15:34:56 -0500

    scaffold contract

--- contracts/contracts/Publius.sol
+++ contracts/contracts/Publius.sol
@@ -1,8 +1,8 @@
-//SPDX-Indentifier: UNLICENSED;
-pragma solidity 0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-import { ERC721Upgradeable } "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializeable.sol";
+import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract Publius is ERC721Upgradeable, Initializable {
 	
@@ -12,16 +12,44 @@ contract Publius is ERC721Upgradeable, Initializable {
 	mapping(address => uint256[]) public minterOwnedTokens;
 
 	struct Book {
-		Chapter[] chapters;
+		string name;
+		string author;
+		string coverImage;
+		mapping(Section => Chapter[] public sectionChapters;
+		mapping(Chapter => Page[]) public chapterPages;
+	}
+
+	struct Section {
+		string name;
+		uint256 id;
+		mapping(Chapter => Page[]) public 
+	}
+
+	struct Chapter {
+		string name;
+		uint256 id;
+		Page[] public pages;
+	}
+
 
 
 	function initialize(string _publicationName, address _author) public Initializer {
 	
 	}
 
-	function tokenURI(uint256 _tokenId) public returns (string uri) {
+	function addChapter(uint256 _section) {
 
 	}
 
+	function addPage(uint256 _section, uint256 _chapter) {
+	
+	}
+
+	function tokenURI(uint256 _tokenId) public returns (string uri) {
+
+	}
+	
+	function getChapter
 
 
+}
