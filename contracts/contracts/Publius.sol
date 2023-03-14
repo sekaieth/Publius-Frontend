f9fa42e05e4b354058e2878e67e10666c8b3fc25
sekaieth <sekaieth@proton.me>
2023-03-14 12:03:16 -0500

    maybe tokenURI works?  Need to start tests.

--- contracts/contracts/Publius.sol
+++ contracts/contracts/Publius.sol
@@ -5,6 +5,8 @@ import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import 'base64-sol/base64.sol';
+
 
 contract Publius is 
 	Initializable, 
@@ -19,23 +21,27 @@ contract Publius is
 	mapping(address => uint256[]) public minterOwnedTokens;
 	// @dev Publication section storage
 	mapping(uint256 => Section) public sections;
+	// @dev Publication chapter storage
+	mapping(uint256 => Chapter) public chapters;
 
 	string public publicationName;
 	address public publicationAuthor;
 	string public publicationCoverImage;
+	uint256 sectionCount;
 
 	struct Section {
 		string sectionName;
 		uint256 sectionId;
 		string sectionImage;
-		mapping(uint256 => Chapter) chapters;
+		uint256[] chapters;
 	}
 	
 	struct Chapter {
 		string chapterName;
 		uint256 chapterId;
 		string chapterImage;
-		Page[] pages;
+		mapping(uint256 => Page) pages;
+		uint256 pageCount;
 	}
 
 	struct Page {
@@ -61,69 +67,51 @@ contract Publius is
 
 	}
 
-	// Add section with all chapter and page data
-	function addSection(string calldata _sectionName, uint256 _sectionId, string calldata _sectionImage, string[] calldata _chapterNames, string[] calldata _chapterImages, uint256[] calldata _chapterIds, string[] calldata _pageNames, string[] calldata _pageContent) public onlyOwner {
-		// Load new section
-		Section storage section = sections[_sectionId];
+	function addSection(string calldata _sectionName, uint256 _sectionId, string calldata _sectionImage, bytes calldata _chapterInfo) public onlyOwner {
 
-
-		// Load each chapter with pages
-		for (uint256 i = 0; i < _chapterNames.length; i++) {
-			Page[] storage pages = section.chapters[_chapterIds[i]].pages;
-			// Loop through pages and add to chapter
-			for (uint256 j = 0; j < _pageNames.length; j++) {
-				pages[j] = Page(
-					_pageNames[j],
-					j + 1,
-					_pageContent[j]
-				);
-			}
-			section.chapters[_chapterIds[i]] = Chapter(
-				{
-				chapterName: _chapterNames[i],
-				chapterId: _chapterIds[i],
-				// Check if _chapterImage is not an empty string calldata then set sectionImage to _chapterImage
-				chapterImage: keccak256(abi.encode(_chapterImages[i])) != "" ?  _chapterImages[i] : "",
-				pages: pages
-				}
-			);
-		}
+		// Decode chapter info
+		(
+			string[] memory _chapterNames, 
+			string[] memory _chapterImages, 
+			uint256[] memory _chapterIds, 
+			string[] memory _pageNames, 
+			string[] memory _pageContent
+		) = abi.decode(_chapterInfo, (string[], string[], uint256[], string[], string[]));
 
 		// Fill in section data
-		section.sectionName = _sectionName;
-		section.sectionId = _sectionId;
-		section.sectionImage = _sectionImage;
+		sections[_sectionId].sectionName = _sectionName;
+		sections[_sectionId].sectionId = _sectionId;
+		sections[_sectionId].sectionImage = _sectionImage;
+		sections[_sectionId].chapters = _chapterIds;
+
+		// Load each chapter with pages
+		for(uint256 i = 0; i < _chapterNames.length; i++) {
+			addChapter(_chapterNames[i], _chapterImages[i], _chapterIds[i], _pageNames, _pageContent);
+		}
+
+		sectionCount++;
 	}
 	
 	/*
 	* @dev Add a new chapter to the publication
 	* @dev _chapterImage is optional - just use an empty string calldata if you don't want to add an image
-	* @param _sectionName The name of the section
-	* @param _chapterImage The image of the section
+	* @param _chapterName The name of the chapter
+	* @param _chapterImage The image of the chapter
 	*/
-	function addChapter(uint256 _section, string calldata _chapterName, string calldata _chapterImage, uint256 _chapterId, string[] calldata _pageNames, string[] calldata _pageContent) public onlyOwner {
-		// Get Existing Section
-		Section storage section = sections[_section];
-		Page[] storage pages = section.chapters[_chapterId].pages; 
+	function addChapter(string memory _chapterName, string memory _chapterImage, uint256 _chapterId, string[] memory _pageNames, string[] memory _pageContent) public onlyOwner {
+		Chapter storage chapter = chapters[_chapterId];
 		for (uint256 i = 0; i < _pageNames.length; i++) {
-			pages[i] = Page(
+			chapter.pages[i + 1] = Page(
 				_pageNames[i],
 				i + 1,
 				_pageContent[i]
 			);
+			chapter.pageCount++;
 		}
 
-		section.chapters[_chapterId] = Chapter( 
-			{
-			chapterName: _chapterName,
-			chapterId: _chapterId,
-			// Check if _chapterImage is not an empty string calldata then set sectionImage to _chapterImage
-			chapterImage: keccak256(abi.encode(_chapterImage)) != "" ?  _chapterImage : "",
-			pages: pages			}
-		);
-			
-				
-
+		chapter.chapterName = _chapterName;
+		chapter.chapterId = _chapterId;
+		chapter.chapterImage = keccak256(abi.encode(_chapterImage)) != "" ?  _chapterImage : "";
 	}
 
 	/*
@@ -131,17 +119,134 @@ contract Publius is
 	* @param _section The section the page belongs to
 	* @param _chapter The chapter the page belongs to
 	*/
-	function addPage(uint256 _section, uint256 _chapter, string calldata _pageName, string calldata _pageContent) public onlyOwner {
-		Chapter storage chapter = sections[_section].chapters[_chapter];
+	function addPage(uint256 _chapter, string calldata _pageName, string calldata _pageContent) public onlyOwner {
+		Chapter storage chapter = chapters[_chapter];
 
-		chapter.pages.push(Page(
+		chapter.pages[chapter.pageCount + 1] = (Page(
 			_pageName,
-			chapter.pages.length + 1,
+			chapter.pageCount + 1,
 			_pageContent
 		));
+		chapter.pageCount++;
 	}
 
-	function tokenURI(uint256 _tokenId) public view override returns (string memory uri) {
 
+	
+    function uint2str(uint256 num) internal pure returns (string memory) {
+        if (num == 0) {
+            return "0";
+        }
+        uint256 j = num;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        while (num != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(num % 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            num /= 10;
+        }
+        return string(bstr);
+    }
+
+	function tokenURI(uint256 tokenId) public view override returns (string memory) {
+		require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+		// Build the JSON structure
+		string memory json = '{ "Sections": [';
+
+		for (uint256 i = 0; i < sectionCount; i++) {
+			// Get the section details
+			Section storage section = sections[i];
+
+			// Build the section JSON
+			string memory sectionJson = 
+				string(abi.encodePacked(
+					'{ "sectionId": "', 
+					uint2str(section.sectionId), 
+					'", "sectionName": "', 
+					section.sectionName, 
+					'", "sectionDescription": "', 
+					'", "sectionImage": "', 
+					section.sectionImage,
+					'", "chapters": ['
+				));
+
+			// Build the chapter JSON
+			string memory chapterJson = "";
+			for (uint256 j = 0; j < section.chapters.length; j++) {
+				// Get the chapter details
+				Chapter storage chapter = chapters[section.chapters[j]];
+
+				// Build the chapter JSON
+				chapterJson = string(abi.encodePacked(
+					chapterJson,
+					'{ "chapterId": "', 
+					uint2str(chapter.chapterId), 
+					'", "chapterName": "', 
+					chapter.chapterName, 
+					'", "chapterDescription": "', 
+					'", "chapterImage": "', 
+					chapter.chapterImage,
+					'", "pages": ['
+				));
+
+				// Build the page JSON
+				string memory pageJson = "";
+				for (uint256 k = 0; k < chapter.pageCount; k++) {
+					// Get the page details
+					Page storage page = chapter.pages[k];
+
+					// Build the page JSON
+					pageJson = string(abi.encodePacked(
+						pageJson,
+						'{ "pageId": "', 
+						uint2str(page.pageId), 
+						'", "pageName": "', 
+						page.pageName, 
+						'", "pageContent": "', 
+						page.pageContent,
+						'"}'
+					));
+
+					// Add a comma to separate pages
+					if (k < chapter.pageCount - 1) {
+						pageJson = string(abi.encodePacked(pageJson, ","));
+					}
+				}
+
+				// Add the page JSON to the chapter JSON
+				chapterJson = string(abi.encodePacked(chapterJson, pageJson, "]"));
+
+				// Add a comma to separate chapters
+				if (j < section.chapters.length - 1) {
+					chapterJson = string(abi.encodePacked(chapterJson, ","));
+				}
+			}
+
+			// Add the chapter JSON to the section JSON
+			sectionJson = string(abi.encodePacked(sectionJson, chapterJson, "]"));
+
+			// Add the section JSON to the overall JSON
+			json = string(abi.encodePacked(json, sectionJson));
+
+			// Add a comma to separate sections
+			if (i < sectionCount - 1) {
+				json = string(abi.encodePacked(json, ","));
+			}
+		}
+
+		// Close the JSON structure
+		json = string(abi.encodePacked(json, ']}'));
+		
+		// Concatenate the base URI and the JSON structure to form the complete URI
+		string memory baseURI = _baseURI();
+		return string(abi.encodePacked(baseURI, json));
 	}
+
 }
