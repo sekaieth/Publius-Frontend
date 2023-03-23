9091788f05d0369c2913b2c65cb1f6af35d0651a
sekaieth <sekaieth@proton.me>
2023-03-23 12:41:26 -0500

    add Wagmi config to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Editor } from './Editor';
+import { WagmiConfig, createClient, chain, configureChains } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const { chains, provider } = configureChains(
+  [chain.hardhat],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: 'http://localhost:8545',
+      })
+    })
+  ]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider: provider,
+});
 
 function App() {
   return (
     <div>
-      <Editor />
+      <WagmiConfig client={client} >
+        <Editor />
+      </WagmiConfig>
     </div>
   );
 }
