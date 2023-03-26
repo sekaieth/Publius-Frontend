ab86a7512b52638dded8e5ed33d9c158ac99cbc8
sekaieth <sekaieth@proton.me>
2023-03-25 19:58:03 -0500

    update navigate hook

--- src/Components/Editor/Editor.tsx
+++ src/Components/Editor/Editor.tsx
@@ -67,7 +67,7 @@ export function Editor() {
     <>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "3%", flexDirection: "column" }}>
         <ConnectButton />
-        { address && !isDisconnected && <button onClick={() => navigate("Reader")}>Read a Publication</button> }
+        { address && !isDisconnected && <button onClick={() => navigate("/Reader")}>Read a Publication</button> }
       </div>
       <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
         <img src={PubliusLogo}></img>
